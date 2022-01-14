7f78fd238f5cdbf24b2dfb2ccb61ce2d4031fe73
anirudh <anirudhs001@gmail.com>
2022-01-14 21:24:25 +0530

    Added pong solver using DQN

--- atari_solver.py
+++ atari_solver.py
@@ -1,5 +1,6 @@
-import collections
+from os import replace
 import gym
+from numpy.core.fromnumeric import repeat
 import tensorboardX
 import time
 import numpy as np
@@ -8,173 +9,255 @@ from matplotlib import pyplot as plt
 import matplotlib.animation as animation
 import torch
 from torch import nn, optim
-from torch.nn.modules import padding
+import argparse
+import collections
 
-ENV_NAME = "MsPacman-v0"
-BUFFER_MAXLEN = 5000
-N1 = 100
-EPISODE_LENGTH = 500
+ENV_NAME = "PongNoFrameskip-v4"
+GAMMA = 0.99
+BATCH_SIZE = 32
+LR = 1e-4
+BUFFER_MAXLEN = 10 ** 4
+REPLAY_START_SIZE = 10 ** 4
+REPEAT_ACTIONS_COUNT = 2
+TARGET_NET_UPDATE_RATE = 1000
+MAX_EPSILON = 1.0
+MIN_EPSILON = 2e-2
+EPSILON_DECAY_LAST_ITR = 1e5
+THRESHOLD_SCORE = 18.0
 
 
 class Agent:
-    def __init__(self, n_actions) -> None:
-        self.replay_buffer = collections.deque(maxlen=BUFFER_MAXLEN)
-        self.qmodel_main = self.QModel(n_actions)
-        self.qmodel_target = self.QModel(n_actions)
-        self.qmodel_target.eval()
-        self.color = np.array(
-            [210, 164, 74]
-        ).mean()  # constant color used by mod_image().
-        # this color is replaced with 0 in image to improve contrast
-        self.epsilon = 0.2
-        self.optim = optim.Adam(self.qmodel_main.parameters(), lr=1e-2)
-        self.batch_size = 16
-        self.total_reward = 0.0
+    def __init__(self, n_actions, dev="cpu") -> None:
+        self.buffer_maxlen = BUFFER_MAXLEN
+        self.replay_buffer_initial = collections.deque(maxlen=BUFFER_MAXLEN//2)
+        self.replay_buffer = collections.deque(maxlen=BUFFER_MAXLEN - BUFFER_MAXLEN//2)
+        self.qmodel_main = self.Create_q_model(n_actions).to(dev)
+        self.qmodel_targ = self.Create_q_model(n_actions).to(dev)
+        self.color = np.array([144, 72, 17]).mean() # this color is replaced with 0 in image to improve contrast
+        self.epsilon = MAX_EPSILON
+        self.optim = optim.Adam(self.qmodel_main.parameters(), lr=LR)
+        self.batch_size = BATCH_SIZE
+        self.ep_score = 0.0
+        self.objective = nn.MSELoss()
+        self.dev = dev
+        self.gamma = GAMMA
 
-    def QModel(n_actions, in_h=88, in_w=80, in_c=1):
+    def Create_q_model(self, n_actions):
         """
         Neural net to estimate Qvalues for all states and actions.
         takes in a state, outputs Qvalues for actions.
         """
-        net = nn.Sequential(
-            nn.Conv2d(in_c, 16, 8, stride=4),
-            nn.LeakyReLU(),
-            nn.Conv2d(16, 32, 4, stride=2),
-            nn.LeakyReLU(),
+        return nn.Sequential(
+            nn.Conv2d(1, 32, 8, stride=4),
+            nn.ReLU(),
+            nn.Conv2d(32, 64, 3, stride=2),
+            nn.ReLU(),
+            nn.Conv2d(64, 64, 3, stride=1),
+            nn.ReLU(),
             nn.Flatten(),
-            nn.Linear(2304, 1600),
-            nn.LeakyReLU(),
-            nn.Linear(1600, n_actions),
+            nn.Linear(3136, 512),
+            nn.ReLU(),
+            nn.Linear(512, n_actions),
         )
-        return net
 
-    def mod_image(self, state):
-        """
-        Function to transform raw screen to smaller image before passing to the neural net.
-        output = 88x80x1 image
-        """
-        image = state[1:176:2, ::2]
-        image = image.mean(axis=2)
-        image[image == self.color] = 0
-        # normalize image
-        image = (image - 128) / 128 - 1
-        # print(image.shape)
-        # reshape: add channel dimension
-        image = torch.tensor(np.expand_dims(image.reshape(88, 80, 1), axis=0)).view(
-            2, 1, 0
-        )
-        return image
-
-    def update_target_network(self):
-        self.qmodel_target.load_state_dict(self.qmodel_main.state_dict())
-
-    def play_episode(self, env, s):
-        a = self.get_action(s, env)
-        new_s, r, is_done, _ = env.step(a)
-        self.add_transn(s, a, new_s, r, is_done)
-        if is_done:
-            s = env.reset()
-            self.total_reward = 0
-        else:
-            s = new_s
-            self.total_reward += r
+    def mod_img(self, s):
+        s = s[34:194:2, ::2]
+        s = np.mean(s, axis=2)
+        s[s == self.color] = 0
+        s = (s - 127.5) / 127.5
+        s = torch.FloatTensor(s).unsqueeze(0)
         return s
 
-    def add_transn(self, s, a, new_s, r, done):
-        self.replay_buffer.append(
-            (self.mod_image(s), a, self.mod_image(new_s), r, done)
-        )
-
-    def get_action(self, s, env):
-        """
-        Take an action acc to epsilon greedy policy
-        """
-
-        # 1. select action with epsilon greedy policy
-        if np.random() < self.epsilon:
-            return env.action_space.sample()
+    def add_transn(self, s, next_s, r, a, is_done):
+        s_mod = self.mod_img(s)
+        next_s_mod = self.mod_img(next_s)
+        if self.replay_buffer_initial.maxlen > len(self.replay_buffer_initial):
+            self.replay_buffer_initial.append((s_mod, next_s_mod, r, a, is_done))
         else:
-            # 2. find a which maximizes Q_model(s, a)
-            best_a = None
-            s_mod = self.mod_image(s).unsqueeze(0)
-            with torch.no_grad():
-                q_m = self.qmodel_main(s_mod).squeeze(0)
-            best_a = torch.argmax(q_m)
-            return best_a
+            self.replay_buffer.append((s_mod, next_s_mod, r, a, is_done))
 
-    def train(self):
-        class criterion(nn.Module):
-            def __init__(self) -> None:
-                super().__init__()
+    @torch.no_grad()
+    def get_action(self, s, env : gym.Env):
+        a = None
+        if np.random.random() < self.epsilon:
+#             a = env.action_space.sample()
+            a = np.random.randint(low = 0, high = 2)
+        else:
+            s_mod = self.mod_img(s).unsqueeze(0).to(self.dev)
+            q_m = self.qmodel_main(s_mod).squeeze()
+            a = torch.argmax(q_m).item()
+        return a + 2
 
-            def forward(self, actions, rewards, q_ms, dones):
-                loss = 0.0
-                for i in range(self.batch_size):
-                    yi = rewards[i]
-                    if not dones[i]:
-                        yi += q_ts[i][best_actions_next[i]]
-                    loss += yi - q_ms[i][actions[i]]
-                loss = loss ** 2
-                loss /= self.batch_size
-                return loss
+    @torch.no_grad()
+    def play_k_steps(self, s, env : gym.Env, repeat_steps=1, save_transn=False):
+        a = self.get_action(s, env)
+        ep_score = 0
+        r = 0.
+        for _ in range(repeat_steps):
+            next_s, _r, is_done, _ = env.step(a)
+            r += _r
+            if is_done: 
+                break
 
-        if len(self.replay_buffer) < self.batch_size:
-            return
-        states, actions, next_states, rewards, dones = self.get_batch()
+        self.ep_score += r
+        if save_transn:
+            self.add_transn(s, next_s, r, a, is_done)
+        ep_score = self.ep_score
+        if is_done: 
+            self.ep_score = 0
+            next_s = env.reset()
+        return next_s, is_done, ep_score
+
+    def get_batch(self, batch_size=32):
+        def get_batch_from_buff(buff, batch_size, s, next_s, r, a, is_done):
+            minibatch = random.sample(buff, batch_size)
+            for sample in minibatch:
+                s.append(sample[0])
+                next_s.append(sample[1])
+                r.append(sample[2])
+                a.append(sample[3])
+                is_done.append(sample[4])
+            return s, next_s, r, a, is_done
+        s, next_s, r, a, is_done = [], [], [], [], []
+        get_batch_from_buff(self.replay_buffer_initial, batch_size//2, s, next_s, r, a, is_done) 
+        get_batch_from_buff(self.replay_buffer, batch_size - batch_size//2, s, next_s, r, a, is_done) 
+        s = torch.stack(s, dim=0).to(self.dev)
+        next_s = torch.stack(next_s, dim=0).to(self.dev)
+        r = torch.tensor(r).to(self.dev)
+        a = torch.tensor(a).to(self.dev)
+        is_done = torch.BoolTensor(is_done).to(self.dev)
+        return s, next_s, r, a, is_done
+
+    def train(self, repeat_steps=1):
+        if len(self.replay_buffer_initial) + len(self.replay_buffer) < REPLAY_START_SIZE:
+            return 0
         self.optim.zero_grad()
-        q_ms = self.qmodel_main(states)
+        s, next_s, r, a, is_done = self.get_batch(self.batch_size)
+        q_ms = self.qmodel_main(s)
+        q_m = torch.gather(q_ms, 1, (a - 2).unsqueeze(-1)).squeeze()
         with torch.no_grad():
-            q_ms_next = self.qmodel_main(next_states)
-            best_actions_next = torch.argmax(q_ms_next, dim=1)
-            q_ts = self.qmodel_target(next_states)
-
-        loss = criterion(actions, rewards, q_ms, dones)
+            best_a_next = torch.argmax(self.qmodel_main(next_s), dim=1) 
+            q_ts = self.qmodel_targ(next_s)
+            q_t = torch.gather(q_ts, 1, best_a_next.unsqueeze(-1)).squeeze()
+            q_t[is_done] = 0.0
+            q_t = q_t.detach()
+        y = r + (self.gamma ** repeat_steps * q_t)
+        loss = self.objective(q_m, y)
         loss.backward()
-        optim.step()
+        self.optim.step()
 
-    def get_batch(self):
-        minibatch = random.sample(self.replay_buffer, self.batch_size)
-        return minibatch
+        return loss.item()
+
+    def save_net(self, path, epoch):
+        model_state = {
+            "epoch": epoch,
+            "model": self.qmodel_main.state_dict(),
+            "optim": self.optim.state_dict(),
+        }
+        path = f"{path}/model_{epoch}.pt"
+        torch.save(model_state, path)
+
+    def load_net(self, path):
+        model_state = torch.load(path, map_location=self.dev) 
+        self.qmodel_main.load_state_dict(model_state["model"])
+        self.optim.load_state_dict(model_state["optim"])
+        return model_state["epoch"]
+
+    def update_targ_net(self):
+        self.qmodel_targ.load_state_dict(self.qmodel_main.state_dict())
 
 
-## Utility function to update output window with new state
-def show_game_screen(*args):
-    env = args[1]
-    im = args[2]
-    state, _, _, _ = env.step(env.action_space.sample())
-    im.set_array(state)
-    return (im,)
-
-
-if __name__ == "__main__":
-    pass
-    # Main loop here
-    env = gym.make(ENV_NAME)
-    agent = Agent(env.action_space.n)
+def train(env: gym.Env, agent: Agent):
     writer = tensorboardX.SummaryWriter(comment="-pacman_doubleQlearning")
-
     state = env.reset()
+
     print("Starting training")
     episode_num = 0
-    while True:
-        # 1. loop N times
-        while _ in range(EPISODE_LENGTH):
-            # 1. run agent for 1 iteration and update replay buffer
-            state, total_reward, is_done = agent.play_episode(env)
-            if is_done:
-                # print(f"reward for episode = {total_reward}")
-                writer.add_scalar("episode_reward", total_reward, episode_num)
-                episode_num += 1
-            # 2. train agent
-            agent.train()
-        # 2. make target network = main network
-        agent.update_target_network()
+    time_step = 0
+    reward_history = []
+    best_reward = None
+    while episode_num < 1000:
+        # 1. run agent for k iteration and update replay buffer
+        state, ep_done, ep_score = agent.play_k_steps(state, env, REPEAT_ACTIONS_COUNT, save_transn=True)
+        if ep_done:
+            reward_history.append(ep_score)
+            print(f"reward for episode [{episode_num:.2f}] : mean reward = {np.mean(reward_history[-20:]):.2f}, r = {ep_score}, epsilon = {agent.epsilon:.2f}")
+            writer.add_scalar("episode_reward", ep_score, episode_num)
+            
+            if not best_reward or best_reward < ep_score:
+                agent.save_net(".", episode_num)
+                best_reward = ep_score
+                print(f"New best Score:{best_reward}")
+                if best_reward > THRESHOLD_SCORE:
+                    print("Stopping trainig")
+                    break
+            episode_num += 1
+        # 2. train agent
+        loss = agent.train(REPEAT_ACTIONS_COUNT)
+        writer.add_scalar("train_loss", loss, time_step)
+        agent.epsilon = max(
+            MIN_EPSILON, MAX_EPSILON - time_step / EPSILON_DECAY_LAST_ITR
+        )
+        writer.add_scalar("epsilon", agent.epsilon, time_step)
+        time_step += 1
+        # 3. make target network = main network
+        if time_step % TARGET_NET_UPDATE_RATE == 0:
+            agent.update_targ_net()
+            # print("updated target net")
         # exit if avg score > some threshold
     writer.close()
     # run agent on environment and show output
 
-    # fig, ax = plt.subplots()
-    # im = plt.imshow(state, animated=True)
-    # fargs = (env, im)
-    # ani = animation.FuncAnimation(fig, show_game_screen, fargs=fargs, interval = 10, blit=True)
-    # plt.show()
+
+def run(env, agent: Agent, m):
+    state = None
+    mod_state = None
+    ## Utility function to update output window with new state
+    def show_game_screen(*args):
+        im = args[1]
+        im.set_array(state)
+        return (im,)
+
+    state = env.reset()
+    mod_state = agent.mod_img(state).permute(1,2,0)
+    fig, ax = plt.subplots()
+    im = plt.imshow(mod_state, animated=True)
+    plt.ion()
+    fargs = (im,)
+    ani = animation.FuncAnimation(
+        fig, show_game_screen, fargs=fargs, interval=100, blit=True
+    )
+    plt.show()
+    if m == "run-agent":
+        agent.epsilon = 0
+    elif m == "run-random":
+        agent.epsilon = 1
+    agent.load_net("model_209.pt")
+    while True:
+        state, is_done, score = agent.play_k_steps(state, env, repeat_steps=REPEAT_ACTIONS_COUNT, save_transn=False)
+        mod_state = agent.mod_img(state).permute(1,2,0)
+        plt.pause(0.001)
+
+
+if __name__ == "__main__":
+    # Main loop here
+    print(f"Selected Env : {ENV_NAME}")
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--m",
+        default="train",
+        choices=["run-agent", "run-random", "train"],
+        help="""
+            run-random : play randomly,
+            run-agent : play using trained model,
+            train : train to train the model
+            """,
+    )
+    env = gym.make(ENV_NAME)
+    agent = Agent(2, dev="cpu")
+    args = parser.parse_args()
+
+    if args.m == "train":
+        train(env, agent)
+    else:
+        run(env, agent, args.m)
